71d9ae18dc1d402f9cf9cc1bc9d558a93b545915
AMEMELYANOV <emelyanov.am@gmail.com>
2024-10-14 22:02:54 +0300

    Добавил каркас для rest-emulator

---
+++ emulators/rest-emulator/src/test/java/org/examples/rest/RestEmulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.examples.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEmulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
